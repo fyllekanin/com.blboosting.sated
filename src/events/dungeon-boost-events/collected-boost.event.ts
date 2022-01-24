eaac958d7542e8067358fefd5bb5edc155eb019c
Eric <eric.christensen94@gmail.com>
2022-01-24 16:10:10 +0100

    Fix collect permission

--- src/events/dungeon-boost-events/collected-boost.event.ts
+++ src/events/dungeon-boost-events/collected-boost.event.ts
@@ -1,5 +1,5 @@
 import { IEvent } from '../event.interface';
-import { Client, MessageReaction, TextChannel, User } from 'discord.js';
+import { Client, MessageReaction, Permissions, TextChannel, User } from 'discord.js';
 import { DiscordEvent } from '../../constants/discord-event.enum';
 import { EmojiReaction } from '../../constants/emoji.enum';
 import { ConfigEnv } from '../../config.env';
@@ -19,7 +19,8 @@ export class CollectedBoostEvent implements IEvent {
     async run(client: Client, messageReaction: MessageReaction, user: User): Promise<void> {
         const guild = await client.guilds.fetch(ConfigEnv.getConfig().DISCORD_GUILD);
         const channel = await guild.channels.fetch(messageReaction.message.channelId) as TextChannel;
-        if (!await this.isApplicable(channel, messageReaction, user)) {
+        const permissions = channel.permissionsFor(user.id);
+        if (!await this.isApplicable(channel, messageReaction, user, permissions)) {
             return;
         }
 
@@ -43,14 +44,13 @@ export class CollectedBoostEvent implements IEvent {
         return DiscordEvent.MessageReactionAdd;
     }
 
-    private async isApplicable(channel: TextChannel, messageReaction: MessageReaction, user: User): Promise<boolean> {
-        if (channel.parent.id !== ConfigEnv.getConfig().DUNGEON_BOOST_CATEGORY) {
+    private async isApplicable(channel: TextChannel, messageReaction: MessageReaction, user: User, permissions: Readonly<Permissions>): Promise<boolean> {
+        if (!(await this.boostRepository.isBoostChannel(channel.id)) && channel.id !== ConfigEnv.getConfig().DUNGEON_COLLECTOR_CHANNEL) {
             return false;
         }
 
-        const permissions = channel.permissionsFor(user.id);
         return !user.bot &&
-            permissions.has(ConfigEnv.getConfig().DUNGEON_BOOST_MANAGE_PERMISSION) &&
+            permissions.has(ConfigEnv.getConfig().DUNGEON_BOOST_COLLECT_PERMISSION) &&
             messageReaction.emoji.name === EmojiReaction.MONEY_BAG;
     }
 }
